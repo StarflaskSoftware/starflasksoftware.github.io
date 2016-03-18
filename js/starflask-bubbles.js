00969166be4b69286e49a83dd89c5c350a55a76f
andy <andy@localhost.localdomain>
2016-03-18 16:46:12 -0400

    added animated bubbled

--- js/starflask-bubbles.js
+++ js/starflask-bubbles.js
@@ -1,4 +1,5 @@
-var mainCanvas = document.querySelector("#starflask-bubbles").getElementsByTagName('canvas')[0];;
+var canvasContainer = document.querySelector("#starflask-bubbles");
+var mainCanvas = canvasContainer.getElementsByTagName('canvas')[0];;
 var mainContext = mainCanvas.getContext("2d");
 
 var canvasWidth = mainCanvas.width;
@@ -8,28 +9,90 @@ var requestAnimationFrame = window.requestAnimationFrame ||
                             window.mozRequestAnimationFrame ||
                             window.webkitRequestAnimationFrame ||
                             window.msRequestAnimationFrame;
+
+var bubble_count = 15;
+var bubbles = [];
 var angle = 0;
 
-function drawCircle() {
+function spawnBubbles()
+{
+  for(var i = 0; i< bubble_count;i++)
+  {
+    var new_bubble = getNewBubble();
+    bubbles.push(new_bubble);
+  }
+
+}
+
+function resetBubble(bubble_index)
+{
+  bubbles[bubble_index] = getNewBubble();
+
+}
+
+function getNewBubble()
+{
+  return {color:randomColor({  luminosity: 'light'}) , radius:(15*Math.random()), x: canvasWidth/2, y: canvasHeight , x_speed: (Math.random() / 5), y_speed: (0.5+Math.random())  ,life:0};
+}
+
+var frameCount = 0;
+
+
+function updateBubbles() {
+
   mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
 
+
+  for(var i in bubbles )
+  {
+    var bubble = bubbles[i];
+
+    // draw the circle
+    mainContext.beginPath();
+
+
+    mainContext.arc(bubble.x, bubble.y, bubble.radius, 0, Math.PI * 2, false);
+    mainContext.closePath();
+
+    // color in the circle
+    mainContext.fillStyle = bubble.color;
+    mainContext.fill();
+
+
+      bubble.y -= bubble.y_speed;
+      bubble.x += bubble.x_speed;
+
+      if( bubble.y < 0)
+      {
+        resetBubble(i);
+      }
+
+      if(Math.random() < 0.2)
+      {
+        bubble.x_speed += (-0.5 + Math.random())/5;
+      }
+
+      console.log('drawing circle ' + bubble.x_speed) ;
+
+  }
+
+
+
+//this breaks errthing
+//  mainCanvas.width  = canvasContainer.offsetWidth;
+//  mainCanvas.height = canvasContainer.offsetHeight;
+
+
+
+
      // color in the background
-     mainContext.fillStyle = "#EEEEEE";
-     mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+     //mainContext.fillStyle = "#EEEEEE";
+    // mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
 
-     // draw the circle
-     mainContext.beginPath();
 
-       var radius = 25 + 150 * Math.abs(Math.cos(angle));
-     mainContext.arc(225, 225, radius, 0, Math.PI * 2, false);
-     mainContext.closePath();
-
-     // color in the circle
-     mainContext.fillStyle = "#006699";
-     mainContext.fill();
-
-       angle += Math.PI / 64;
-
-      requestAnimationFrame(drawCircle);
+  requestAnimationFrame(updateBubbles);
 }
-drawCircle();
+
+
+spawnBubbles();
+requestAnimationFrame(updateBubbles);
